62fdb7e86e858d4346c3188d525997c6304ebe31
Alchemist of the web <nikitakzntcv@gmail.com>
2024-09-12 22:41:04 +0400

    add constructor with initializer_list into MyArray class;

--- homework1/homework1.cpp
+++ homework1/homework1.cpp
@@ -1,12 +1,32 @@
 ﻿#include <iostream>
 #include <stdexcept>
+#include <initializer_list> 
+#include <array>
 
-template <typename T, size_t _size>
+
+template <typename T>
+concept Numeric = std::is_integral_v<T>;
+
+
+template <Numeric T, size_t _size>
 class MyArray {
 private:
     T m_arr[_size];
 public:
     MyArray() {}
+
+    MyArray(const std::initializer_list<T>& init_list) {
+        if (init_list.size() > _size) {
+            throw std::out_of_range("Init list size is out of range");
+        }
+
+        int i = 0;
+        for (auto &el : init_list) {
+            m_arr[i] = el;
+            i++;
+            /*m_arr[i] = init_list[i];*/
+        }
+    }
 
     void fill(T filler) {
         for (int i = 0; i <= _size; i++) {
@@ -14,10 +34,18 @@ public:
         }
     }
 
-    T& operator[](int index) {
-        if (index >= _size || index <= 0)
-            throw std::invalid_argument("value is out of bounce in []");
+    T& operator[](size_t index) {
+        if (index < 0)
+            throw std::invalid_argument("value must be positive");
         return m_arr[index];
+    }
+
+    template <Numeric T, size_t _size>
+    bool operator==(const MyArray<T, _size>& other) {
+        for (int i = 0; i <= _size; i++) {
+            if (m_arr[i] != other[i]) return false;
+        }
+        return true;
     }
 
     T* data() {
@@ -54,5 +82,7 @@ public:
 
 int main() {
     std::cout << "Hello World!\n";
-    MyArray<int, 1> arr;
+    MyArray<int, 3> a {1, 2, 3};
+    MyArray<int, 3> b {1, 2, 3};
+    std::cout << "a == b: " << (a == b) << std::endl;
 }
